e6ed3b479d3fa63560d440e8c1690ba650203f68
ajithbtk <malluflix.official@gmail.com>
2024-04-14 10:00:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ function duplicateImage() {
 
     const originalImage = document.getElementById('img2');
     const clonedImage = originalImage.cloneNode(true);
-    
     const container = document.getElementById('row');
     container.appendChild(clonedImage);
 }
