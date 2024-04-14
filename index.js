fc31abc0db7e93a08d7d8bc4be90bfb437582ae7
ajithbtk <malluflix.official@gmail.com>
2024-04-14 13:20:21 +0530

    Front-end-Project

--- index.js
+++ index.js
@@ -8,3 +8,107 @@ function duplicateImage(id) {
     
 }
 
+
+function averageColor(imageElement, parentElement) {
+ 
+    if (!imageElement || !parentElement) {
+        console.error('Invalid parameters for averageColor function.');
+        return;
+    }
+    // Create the canvas element
+    var canvas
+        = document.createElement('canvas'),
+
+        // Get the 2D context of the canvas
+        context
+            = canvas.getContext &&
+            canvas.getContext('2d'),
+        imgData, width, height,
+        length,
+
+        // Define variables for storing
+        // the individual red, blue and
+        // green colors
+        rgb = { r: 0, g: 0, b: 0 },
+
+        // Define variable for the 
+        // total number of colors
+        count = 0;
+
+    // Set the height and width equal
+    // to that of the canvas and the image
+    height = canvas.height =
+        imageElement.naturalHeight ||
+        imageElement.offsetHeight ||
+        imageElement.height;
+    width = canvas.width =
+        imageElement.naturalWidth ||
+        imageElement.offsetWidth ||
+        imageElement.width;
+
+    // Draw the image to the canvas
+    context.drawImage(imageElement, 0, 0);
+    imageElement.crossOrigin = "anonymous"
+    // Get the data of the image
+    imgData = context.getImageData(
+                0, 0, width, height);
+
+    // Get the length of image data object
+    length = imgData.data.length;
+
+    for (var i = 0; i < length; i += 4) {
+
+        // Sum all values of red colour
+        rgb.r += imgData.data[i];
+
+        // Sum all values of green colour
+        rgb.g += imgData.data[i + 1];
+
+        // Sum all values of blue colour
+        rgb.b += imgData.data[i + 2];
+
+        // Increment the total number of
+        // values of rgb colours
+        count++;
+    }
+
+    // Find the average of red
+    rgb.r
+        = Math.floor(rgb.r / count);
+
+    // Find the average of green
+    rgb.g
+        = Math.floor(rgb.g / count);
+
+    // Find the average of blue
+    rgb.b
+        = Math.floor(rgb.b / count);
+
+
+    var result = ""
+
+    if(rgb.r > rgb.g){
+        if(rgb.r > rgb.b){
+            result = "red";
+        }
+        else{
+            result = "blue"
+        }
+    }
+    else{
+        if(rgb.g > rgb.b){
+            result = "green"
+        }
+        else{
+            result = "blue"
+        }
+    }
+    
+    const para = document.createElement("p");
+    const node = document.createTextNode("The image is"+ result);
+    para.appendChild(node);
+
+    const element = document.getElementById(parentElement);
+    element.appendChild(para);
+}
+
